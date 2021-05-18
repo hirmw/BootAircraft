c49106f182555d6b85343d1ada6eb81990aa0035
rmwhib <richmw1980@gmail.com>
2021-05-18 09:24:03 +0100

    added opensky

--- src/main/java/com/example/newAircraft/aircraft/Enterdata.java
+++ src/main/java/com/example/newAircraft/aircraft/Enterdata.java
@@ -1,5 +1,9 @@
 package com.example.newAircraft.aircraft;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.opensky.api.OpenSkyApi;
+import org.opensky.model.OpenSkyStates;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.util.Locale;
 
 @Component
 public class Enterdata implements CommandLineRunner {
@@ -24,6 +29,16 @@ public class Enterdata implements CommandLineRunner {
 
         var a1 = new Aircraft("787");
         aeroRepository.save(a1);
+
+        OpenSkyApi api = new OpenSkyApi("rmw2020","Labrad0r");
+        OpenSkyStates os = api.getStates(0, null,
+                new OpenSkyApi.BoundingBox(45.8389, 47.8229, 5.9962, 10.5226));
+
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(os);
+        logger.info("hello opensky" + os.);
+
 
     }
 
